f554c42b428f68d2b1abf47873f590eb79ae58f9
Melissa Albarella <moexu13@gmail.com>
2020-12-15 15:16:26 -0800

    finished books.js - all tests passing

--- public/src/books.js
+++ public/src/books.js
@@ -1,10 +1,37 @@
-function findAuthorById(authors, id) {}
+const { findAccountById } = require("./accounts");
 
-function findBookById(books, id) {}
+function findAuthorById(authors, id) {
+  return authors.find(author => author.id === id);
+}
 
-function partitionBooksByBorrowedStatus(books) {}
+function findBookById(books, id) {
+  return books.find(book => book.id === id);
+}
 
-function getBorrowersForBook(book, accounts) {}
+function partitionBooksByBorrowedStatus(books) {
+  let borrowed = [];
+  let inStock = [];
+  books.forEach(book => {
+    // the first borrow record contains the current status
+    if (book.borrows[0].returned) {
+      inStock.push(book);
+    } else {
+      borrowed.push(book);
+    }
+  });
+  return [borrowed, inStock];
+}
+
+function getBorrowersForBook(book, accounts) {
+  let borrowers = [];
+  // limit to top 10 borrowers
+  book.borrows.splice(0, 10).forEach(borrow => {
+    let account = findAccountById(accounts, borrow.id);
+    account.returned = borrow.returned;
+    borrowers.push(account);
+  });
+  return borrowers;
+}
 
 module.exports = {
   findAuthorById,
