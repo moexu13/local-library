05ba5b3b5a8fef8132a3a258e7e9c2134e2b67c6
moexu <moexu13@gmail.com>
2020-12-15 12:01:27 -0800

    transferring project to desktop computer

--- docs/function-instructions.md
+++ docs/function-instructions.md
@@ -3,6 +3,53 @@
 The following functions can be completed in whichever order you choose. There are difficult problems associated with each page, so feel free to skip around as you work your way up to solving more challenging problems!
 
 ---
+Account data structure
+
+{
+  "id": "5f446f2ecfaf0310387c9603",
+  "name": {
+    "first": "Esther",
+    "last": "Tucker"
+  },
+  "picture": "https://api.adorable.io/avatars/75/esther.tucker@zillacon.me",
+  "age": 25,
+  "company": "ZILLACON",
+  "email": "esther.tucker@zillacon.me",
+  "registered": "Thursday, May 28, 2015 2:51 PM"
+}
+
+Author data structure
+
+{
+  "id": 0,
+  "name": {
+    "first": "Lucia",
+    "last": "Moreno"
+  }
+}
+
+Book data structure
+
+{
+  "id": "5f4471327864ee880caf5afc",
+  "title": "reprehenderit quis laboris adipisicing et",
+  "genre": "Poetry",
+  "authorId": 20,
+  "borrows": [
+    {
+      "id": "5f446f2e2a4fcd687493a775",
+      "returned": false
+    },
+    {
+      "id": "5f446f2ebe8314bcec531cc5",
+      "returned": true
+    },
+    {
+      "id": "5f446f2ea508b6a99c3e42c6",
+      "returned": true
+    }
+  ]
+}
 
 ### Account functions
 
--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,10 +1,42 @@
-function findAccountById(accounts, id) {}
+function findAccountById(accounts, id) {
+  return accounts.find(account => account.id === id);
+}
 
-function sortAccountsByLastName(accounts) {}
+function sortAccountsByLastName(accounts) {
+  return accounts.sort((acc1, acc2) => acc1.name.last.toLowerCase > acc2.name.last.toLowerCase() ? 1 : -1);
+}
 
-function numberOfBorrows(account, books) {}
+function numberOfBorrows(account, books) {
+  let borrows = 0;
+  books.forEach(book => {
+    let matches = book.borrows.filter(checkout => checkout.id === account.id);
+    borrows += matches.length;
+  });
+  return borrows;
+}
 
-function getBooksPossessedByAccount(account, books, authors) {}
+function getBooksPossessedByAccount(account, books, authors) {
+  let possessedBooks = [];
+  books.forEach(book => {
+    let matches = book.borrows.filter(checkout => checkout.id === account.id && checkout.returned === false);
+    if (matches.length) possessedBooks.push(matches);
+  });
+  possessedBooks.forEach(book => {
+    console.log("here");
+    let author = getAuthorOfBook(book, authors);
+    console.log(author);
+  })
+  // console.log(possessedBooks);
+  // let bookAuthors = [];
+  // authors.forEach(author => {
+
+  // });
+  return possessedBooks;
+}
+
+function getAuthorOfBook(book, authors) {
+  return authors.filter(author => author.id === book.authorId);
+}
 
 module.exports = {
   findAccountById,
